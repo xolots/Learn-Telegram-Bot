26067d9d80f054f4b509aa3643d332ebb162e436
xolots <dakochan64@gmail.com>
2022-09-08 15:03:19 +0700

    change little bit of fact

--- facts.js
+++ facts.js
@@ -1,7 +1,7 @@
 const facts = [
     {
-        fact: "Mother pandas keep contact with their cub nearly 100% of the time during their first month - with the cub resting on her front and remaining covered by her paw, arm or head.",
-        animal: "Panda"
+        fact: "Tahukah kamu kalau kominfo kebobolan data milik rakyat sebanyak 1.3 Miliar Dan Diperjualbelikan Pada Forum Hacker.",
+        animal: "kominfo"
     },
     {
         fact: "The elephant's temporal lobe (the area of the brain associated with memory) is larger and denser than that of people - hence the saying 'elephants never forget'.",
